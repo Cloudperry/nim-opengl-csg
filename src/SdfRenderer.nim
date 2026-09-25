f53e11a9b97d48761f8a98200308903ee86abf55
Roni Hokkanen <roni.hokkanen@protonmail.com>
2026-09-25 14:45:21 +0300

    AI slop cleanup and naming improvements

--- src/SdfRenderer.nim
+++ src/SdfRenderer.nim
@@ -15,7 +15,7 @@ type RenderMode {.size: sizeof(uint32).} = enum
 makeGlObjects(RaiseError, std140Alignment):
   type GpuSdfSceneUniforms = object
     aspect: GLfloat
-    camPos, camForward, camRight, camUp, hitColor, bgColor: Vec3f
+    camPos, camForward, camRight, camUp, bgColor: Vec3f
     fov: GLfloat
     mainLightDirection, mainLightColor, ambientLightColor: Vec3f
     specularExponent: GLfloat
@@ -353,7 +353,6 @@ proc draw(win: Window) =
   # TODO: Move this into the appropriate callback
   let (width, height) = glfw.framebufferSize(win)
   sdfRenderer.sceneUbo.aspect = width / height
-  sdfRenderer.sceneUbo.hitColor = vec3f(1, 1, 1)
   sdfRenderer.sceneUbo.bgColor = vec3f(0.2, 0.3, 0.3)
   sdfRenderer.sceneUbo.fov = 80
   state.camera.setUniforms()
